07e00c859513e805c5d069df9a9b2b311c54a3e8
rogueai <rogueai@users.noreply.github.com>
2023-08-10 14:51:54 +0200

    Added responsive navbar drawer

--- app/src/routes/+layout.svelte
+++ app/src/routes/+layout.svelte
@@ -1,26 +1,55 @@
 <script>
-	// The ordering of these imports is critical to your app working properly
-	import '@skeletonlabs/skeleton/themes/theme-rocket.css';
-	// If you have source.organizeImports set to true in VSCode, then it will auto change this ordering
-	import '@skeletonlabs/skeleton/styles/skeleton.css';
-	// Most of your app wide CSS should be put in this file
-	import '../app.postcss';
-	import { AppShell, AppBar } from '@skeletonlabs/skeleton';
+    // The ordering of these imports is critical to your app working properly
+    import '@skeletonlabs/skeleton/themes/theme-rocket.css';
+    // If you have source.organizeImports set to true in VSCode, then it will auto change this ordering
+    import '@skeletonlabs/skeleton/styles/skeleton.css';
+    // Most of your app wide CSS should be put in this file
+    import '../app.postcss';
+    import {AppShell, AppBar, Drawer, drawerStore} from '@skeletonlabs/skeleton';
+    import Navigation from "../lib/Navigation/Navigation.svelte";
+
+    function drawerOpen() {
+        drawerStore.open({});
+    }
+
 </script>
 
-<!-- App Shell -->
-<AppShell>
-	<svelte:fragment slot="header">
-		<!-- App Bar -->
-		<AppBar>
-			<svelte:fragment slot="lead">
-				<strong class="text-xl uppercase">yt-dlw</strong>
-			</svelte:fragment>
-			<svelte:fragment slot="trail">
+<!-- Overlay -->
+<Drawer>
+    <Navigation/>
+</Drawer>
 
-			</svelte:fragment>
-		</AppBar>
-	</svelte:fragment>
-	<!-- Page Route Content -->
-	<slot />
+<!-- App Shell -->
+<AppShell slotSidebarLeft="bg-surface-500/5 w-0 lg:w-64">
+    <svelte:fragment slot="header">
+        <!-- App Bar -->
+        <AppBar>
+            <svelte:fragment slot="lead">
+                <div class="flex items-center">
+                    <button class="lg:hidden btn btn-sm mr-4" on:click={drawerOpen}>
+                    <span>
+                        <svg viewBox="0 0 100 80" class="fill-token w-4 h-4">
+                            <rect width="100" height="20"/>
+                            <rect y="30" width="100" height="20"/>
+                            <rect y="60" width="100" height="20"/>
+                        </svg>
+                    </span>
+                    </button>
+                    <strong class="text-xl uppercase">yt-dlw</strong>
+                </div>
+            </svelte:fragment>
+
+            <svelte:fragment slot="trail">
+                <a class="btn-icon btn-icon-sm hover:variant-soft-primary"
+                   href="https://github.com/rogueai/yt-dlw" target="_blank" rel="noreferrer"><i
+                        class="fa-brands fa-github text-lg"></i></a>
+            </svelte:fragment>
+        </AppBar>
+    </svelte:fragment>
+    <!-- Left Sidebar Slot -->
+    <svelte:fragment slot="sidebarLeft">
+        <Navigation/>
+    </svelte:fragment>
+    <!-- Page Route Content -->
+    <slot/>
 </AppShell>
--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -1,47 +1,21 @@
-<!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
 
-<div class="container h-full mx-auto flex justify-center">
-    <div class="space-y-10 text-center flex flex-col">
-        <h2 class="h2"></h2>
-        <!-- Animated Logo -->
-        <!-- / -->
-        <form method="POST" action="/download">
-            <div class="flex justify-center space-x-2">
-                <div class="input-group input-group-divider grid-cols-[auto_1fr_auto]">
-                    <input type="search" placeholder="Video ID" name="videoId" value="BaW_jenozKc"/>
-                    <button class="variant-filled-secondary">Download</button>
-                </div>
+<div class="container p-10 space-y-4">
+    <h1>Homepage</h1>
+    <hr/>
+    <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate nostrum, vero explicabo sunt ipsum
+        asperiores. Molestias nostrum mollitia cumque veniam eum error atque, dolores nesciunt voluptas ea consectetur
+        sed delectus.</p>
+    <form method="POST" action="/download">
+        <div class="flex justify-center space-x-2">
+            <div class="input-group input-group-divider grid-cols-[auto_1fr_auto]">
+                <input type="search" placeholder="Video ID" name="videoId" value="BaW_jenozKc"/>
+                <button class="variant-filled-secondary">Download</button>
             </div>
-        </form>
-        <div class="space-y-2">
-
         </div>
-    </div>
+    </form>
 </div>
 
+
 <style lang="postcss">
-    figure {
-        @apply flex relative flex-col;
-    }
 
-    @keyframes glow {
-        0% {
-            @apply bg-primary-400/50;
-        }
-        33% {
-            @apply bg-secondary-400/50;
-        }
-        66% {
-            @apply bg-tertiary-400/50;
-        }
-        100% {
-            @apply bg-primary-400/50;
-        }
-    }
-
-    @keyframes pulse {
-        50% {
-            transform: scale(1.5);
-        }
-    }
 </style>
